437efdeeb0823006f435604ccc1c25978f1fc86a
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2017-05-14 13:41:40 +0900

    Update sample

--- sample/src/main/java/com/os/operando/timber/tree/sample/SampleApplication.java
+++ sample/src/main/java/com/os/operando/timber/tree/sample/SampleApplication.java
@@ -2,6 +2,7 @@ package com.os.operando.timber.tree.sample;
 
 import android.app.Application;
 
+import com.os.operando.timber.tree.crash.slack.CrashReportingSlackTree;
 import com.os.operando.timber.tree.firebasecrashreporting.FirebaseCrashErrorReportTree;
 import com.os.operando.timber.tree.firebasecrashreporting.FirebaseCrashLogTree;
 
@@ -14,5 +15,12 @@ public class SampleApplication extends Application {
         super.onCreate();
 
         Timber.plant(new FirebaseCrashErrorReportTree(), new FirebaseCrashLogTree());
+        Timber.plant(
+                new CrashReportingSlackTree.Builder("your Incoming WebHooks URL for Slack")
+                        .channel("android")
+                        .iconEmoji(":sushi:")
+                        .userName("timber")
+                        .build()
+        );
     }
 }
